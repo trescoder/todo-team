0f06a5dc9354a2812a7731a028de6050149eac5e
Fernando Orozco <forozcomercado@gmail.com>
2022-08-05 14:23:17 -0500

    display member to add to a task

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { Member } from '../interfaces/member';
+import { TodoService } from '../services/todo.service';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css'],
 })
-export class AddTaskComponent implements OnInit {
-  constructor() {}
+export class AddTaskComponent implements OnInit, DoCheck {
+  public members: Member[] = [];
 
-  ngOnInit(): void {}
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {
+    this.members = this.todoService.getMembers();
+  }
+
+  ngDoCheck(): void {
+    this.members = this.todoService.getMembers();
+  }
 
   addTask(form: any) {}
 }
