fd2a5afb7fe033732c437e75248ec4934b7ac4a3
Fernando Orozco <forozcomercado@gmail.com>
2022-08-05 14:00:15 -0500

    Component to add a member added

---
+++ src/app/add-member/add-member.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-member',
+  templateUrl: './add-member.component.html',
+  styleUrls: ['./add-member.component.css'],
+})
+export class AddMemberComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addMember(form: any) {}
+}
