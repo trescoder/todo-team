fc9b6d4469df074425abdc136eef26898e8445f2
Fernando Orozco <forozcomercado@gmail.com>
2022-08-05 14:52:00 -0500

    Now display the tasks with its members

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { Task } from './interfaces/task';
+import { TodoService } from './services/todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
   showTaskForm = true;
+  tasks: Task[] = [];
   title = 'todo-team';
+
+  constructor(private todoService: TodoService) {}
+
+  ngDoCheck(): void {
+    this.tasks = this.todoService.getTasks();
+  }
 }
